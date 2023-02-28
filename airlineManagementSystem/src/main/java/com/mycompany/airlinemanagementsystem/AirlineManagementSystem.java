65a6e0cc5c040fe96639d0d260e13a9508c16443
christopher827 <Christopherocher827@gmail.com>
2023-02-28 17:42:38 +0100

    first commit

---
+++ airlineManagementSystem/src/main/java/com/mycompany/airlinemanagementsystem/AirlineManagementSystem.java
@@ -0,0 +1,8 @@
+
+package com.mycompany.airlinemanagementsystem;
+public class AirlineManagementSystem {
+
+    public static void main(String[] args) {
+        System.out.println(" World!");
+    }
+}
